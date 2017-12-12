bca65aec424a467deac3d5229e4b398b51f107d9
Alejandro Ajenjo <gplp97@gmail.com>
2017-12-12 12:33:02 +0100

    Actividad 3C

--- provision.sh
+++ provision.sh
@@ -6,3 +6,16 @@ sudo rm -rf /var/www/html
 sudo ln -s /vagrant/html/ /var/www/html
 cd /vagrant/html
 git clone https://github.com/Alfirex/sitio1.git
+cd /vagrant/html
+git clone https://github.com/Alfirex/sitio2.git
+
+sudo a2dissite 000-default.conf
+
+sudo rm -rf /etc/apache2/sites-available/
+
+sudo ln -s /vagrant/config /etc/apache2/sites-available
+
+sudo a2ensite sitio1
+sudo a2ensite sitio2
+
+sudo /etc/init.d/apache2 restart
